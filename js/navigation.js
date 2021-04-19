77f147fbaaa5a003aa4e261f34a3d7559f7925a7
sirriah <tereza.dvorakova.3@seznam.cz>
2021-04-19 21:03:02 +0200

    animation menu-toggle

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,22 @@
-var menu = document.querySelector("#mobile-navigation");
-var hamburgerIcon = document.querySelector(".hamburger-menu");
+var menu = document.querySelector('.menu');
+var menuToggle = document.querySelector('#mobile-navigation');
+var hamburgerIcon = document.querySelector('.hamburger-menu');
 
-hamburgerIcon.addEventListener("click", function () {
-  if (menu.classList.contains("hidden")) {
-    menu.classList.remove("hidden");
+
+hamburgerIcon.addEventListener('click', function () {
+  if (menu.classList.contains('open')) {
+    //menuToggle.classList.remove('hidden');
+    menu.classList.remove('open');
+    menuToggle.classList.remove('fade-in');
+    menuToggle.classList.add('fade-out');
+   
   } else {
-    menu.classList.add("hidden");
+    menu.classList.add('open');
+    menuToggle.classList.remove('fade-out');
+    menuToggle.classList.add('fade-in');
+    //menuToggle.classList.add('hidden');
+    
+
+
   }
 });
--- js/slider.js
+++ js/slider.js
@@ -6,11 +6,25 @@ let previousSlide = 0;
 function stepSlide(step){
     previousSlide = currentSlide;
     currentSlide = (((currentSlide + step) % slides.length) + slides.length) % slides.length;
+// test
+/*
+    slides[previousSlide].classList.remove('fade-in');
+    slides[previousSlide].classList.add('fade-out');
+    slideHeadline[previousSlide].classList.remove('fade-in');
+    slideHeadline[previousSlide].classList.add('fade-out');
+
+
+    slides[currentSlide].classList.remove('fade-in');
+    slides[currentSlide].classList.add('fade-out');
+    slideHeadline[currentSlide].classList.remove('fade-in');
+    slideHeadline[currentSlide].classList.add('fade-out');
+*/
 
     slides[previousSlide].classList.add('hidden');
     slideHeadline[previousSlide].classList.add('hidden');
 
     slides[currentSlide].classList.remove('hidden');
     slideHeadline[currentSlide].classList.remove('hidden');
+ 
 }
 
