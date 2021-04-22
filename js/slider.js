fdde3f7cf228035744a51bccbf7ea7b83d665e2b
sirriah <tereza.dvorakova.3@seznam.cz>
2021-04-22 11:47:48 +0200

    subgrid works for firefox

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,7 @@
 const slides = document.getElementsByClassName('slider__image');
 const slideHeadline = document.getElementsByClassName('slide__headline');
+const sliderSection = document.getElementsByClassName('slider');
+
 let currentSlide = 0;
 let previousSlide = 0;
 
@@ -19,12 +21,34 @@ function stepSlide(step){
     slideHeadline[currentSlide].classList.remove('fade-in');
     slideHeadline[currentSlide].classList.add('fade-out');
 */
+//slides[currentSlide].classList.remove('animated');
+slides[currentSlide].style.zIndex = "2";
+slides[previousSlide].classList.add('slide-right');
+document.querySelector('.slide-right').addEventListener('animationend', function() {
+    slides[previousSlide].classList.remove('slide-right');
+    console.log("animace probehla");
+    slides[currentSlide].style.zIndex = "3";
+    slides[previousSlide].style.zIndex = "1";
+    
+});
+/*
+slides[previousSlide].classList.add('hidden');
+    slideHeadline[previousSlide].classList.add('hidden');
 
+    slides[currentSlide].classList.remove('hidden');
+    slideHeadline[currentSlide].classList.remove('hidden');
+*/
+
+/*
     slides[previousSlide].classList.add('hidden');
     slideHeadline[previousSlide].classList.add('hidden');
 
     slides[currentSlide].classList.remove('hidden');
     slideHeadline[currentSlide].classList.remove('hidden');
- 
+ */
 }
-
+/*
+anime({
+    targets: slides[currentSlide]
+});
+*/
